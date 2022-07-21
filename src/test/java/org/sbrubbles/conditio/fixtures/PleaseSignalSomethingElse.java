829a04569f3911c995f84f3032e8e32feae40224
Humberto Anjos <h.anjos@sbrubbles.org>
2022-07-21 18:42:55 -0300

    No need for that (ugly) "equals" anymore.

--- src/test/java/org/sbrubbles/conditio/fixtures/PleaseSignalSomethingElse.java
+++ src/test/java/org/sbrubbles/conditio/fixtures/PleaseSignalSomethingElse.java
@@ -2,17 +2,4 @@ package org.sbrubbles.conditio.fixtures;
 
 import org.sbrubbles.conditio.Condition;
 
-public class PleaseSignalSomethingElse implements Condition {
-  // XXX not my proudest code, but for test purposes, screw it :)
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    return true;
-  }
-
-  @Override
-  public int hashCode() {
-    return 1;
-  }
-}
+public class PleaseSignalSomethingElse implements Condition { /**/ }
